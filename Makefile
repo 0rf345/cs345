d188b7140ece3603abe17f43b4cd87eb7222b429
Orfeas Kalipolitis <csd3285@csd.uoc.gr>
2016-10-24 21:22:26 +0300

    created a2 empty folder

--- Makefile
+++
@@ -1,5 +0,0 @@
-all: main.c helper.h
-	gcc -o cs345sh main.c
-
-clean:
-	rm -f cs345sh
--- helper.h
+++
@@ -1,8 +0,0 @@
-#define VAR_NAME_SIZE 100
-#define ARG_NUMBER 100
-
-struct varBoard {
-	char varKey[VAR_NAME_SIZE];
-	char varVal[ARG_NUMBER][VAR_NAME_SIZE];
-	struct varBoard* next;
-};
