847bcb4312712966fdaf8af018b7aeb722f530b6
n0b0d1 <csd3285@csd.uoc.gr>
2016-11-10 18:40:34 +0200

    had the name wrong.. so more changes all around

---
+++ a3/get_remaining_time.c
@@ -0,0 +1,27 @@
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+#include <asm/uaccess.h>
+#include <linux/t_params.h>
+asmlinkage long sys_get_total_c_time(int pid, struct t_params *t_arguments) {
+	struct task_struct *i;
+	printk("sys_get_total_c_time running. AM:3285\n");
+	if(t_arguments == NULL) {
+		printk("t_arguments was NULL\n");
+		return EINVAL;
+	}
+	if(pid == -1) {
+		printk("PID was -1\n");
+		return EINVAL;
+	}
+	for_each_process(i) {
+		if(i->pid == pid) {
+			t_arguments->remaining_time = i->remaining_time;
+			t_arguments->infinite = i->infinite;
+			printk("Got total_c_time for PID: %d\n", pid);
+			return 0;
+		}
+	}
+	printk("PID: %d was not found to be gotten\n", pid);
+	return EINVAL;
+}
+
